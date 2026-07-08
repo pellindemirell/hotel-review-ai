4f51db479f497e5e3017822d05209b72fa04dce1
pellindemirell <pellindemirell@gmail.com>
2026-07-08 15:28:33 +0300

    feat: add Roles constants in Domain layer

---
+++ src/HotelReviewAI.Domain/Enums/Roles.cs
@@ -0,0 +1,9 @@
+namespace HotelReviewAI.Domain.Enums;
+
+public static class Roles
+{
+    public const string Admin = "Admin";
+    public const string Manager = "Manager";
+    public const string DepartmentUser = "DepartmentUser";
+    public const string MobileUser = "MobileUser";
+}
