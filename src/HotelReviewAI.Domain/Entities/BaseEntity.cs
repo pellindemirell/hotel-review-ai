6367d4d3dfc585b6df66699586f2d60539767a18
pellindemirell <pellindemirell@gmail.com>
2026-07-08 15:36:46 +0300

    feat: add BaseEntity, ValueObject, and DomainException to Domain layer

---
+++ src/HotelReviewAI.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HotelReviewAI.Domain.Entities;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; protected set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+
+    protected BaseEntity()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+    }
+}
